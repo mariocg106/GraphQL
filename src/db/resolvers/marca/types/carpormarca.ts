93c29a184d12d315728446e65d615387d3645267
unknown <mariocg106@gmail.com>
2022-03-07 10:25:04 +0100

    union2

--- src/db/resolvers/marca/types/carpormarca.ts
+++ src/db/resolvers/marca/types/carpormarca.ts
@@ -2,14 +2,14 @@ import { IResolvers } from '@graphql-tools/utils';
 import { Db } from 'mongodb';
 import { Imarca } from './../../../interfaces/Imarca'; 
 import { Icar } from './../../../interfaces/Icar'; 
-import { IResult } from './../../../interfaces/IResult';
+
 
 const typecarpormarca: IResolvers = {
     marca: {
-        carpormarca: async ( root: Imarca, __, context: {db: Db} ): Promise<Icar[]> => {
+        carpormarca: async ( marca: Imarca, __, context: {db: Db} ): Promise<Icar[]> => {
             
             const datacar = await context.db.collection("car").find().toArray() as Icar[];
-            const carList = datacar.filter( (car) => car.marca.indexOf(root.cifm) > -1 );
+            const carList = datacar.filter( (car) => car.cifm.indexOf(marca.cifm) > -1 );
             return carList;
         }
     }
