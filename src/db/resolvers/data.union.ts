5d8e7fed53b4675ac8def63959142077fabaaabf
unknown <mariocg106@gmail.com>
2022-03-08 10:56:16 +0100

    todo

--- src/db/resolvers/data.union.ts
+++ src/db/resolvers/data.union.ts
@@ -3,13 +3,17 @@ import { IResolvers } from "@graphql-tools/utils";
 
 const typesUnionResolvers: IResolvers = {
     car: { 
-    __resolveType(obj: { nombre: string; cifm: string }) {
+    __resolveType(obj: { nombre: string; cifm: string; cifCA: string }) {
      
       if (obj.nombre) {
         return "marca";
       }
       
       if (obj.cifm) {
+        return "car";
+      }
+
+      if (obj.cifCA) {
         return "car";
       }
       return null; 
@@ -20,3 +24,4 @@ const typesUnionResolvers: IResolvers = {
 };
 
 export default typesUnionResolvers;
+
